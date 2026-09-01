e18655e6b398a4b2c4f514448d627bc0713505d9
Shreya Sharma <shreyasharam2007@gmail.com>
2026-09-01 10:49:49 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (63.29%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,16 +1,10 @@
 class Solution {
-    public int sumofdigit(int n){
-        int sum=0;
-        while(n!=0){
-            sum+=n%10;
-            n/=10;
-        }
-        return sum;
-    }
     public int addDigits(int num) {
-        while(num>9){
-            num=sumofdigit(num);
-        }
-        return num;
+        if(num==0) return 0;
+        if(num%9==0) return 9;
+
+
+        return num%9;
+        
     }
 }
